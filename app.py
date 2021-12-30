43a7ddc388e0e81945c324dbf1211a5abd45277b
Rijo George <rijothedeveloper@gmail.com>
2021-12-30 09:00:28 -0800

    Part 0: Set up your environment

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+debug = DebugToolbarExtension(app)
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+connect_db(app)
+
+@app.route("/")
+def show_home():
+    return render_template("index.html")
